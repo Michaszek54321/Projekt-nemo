87657d050d55186c08d209f53a6bdf5093180d4a
michaszek <you@example.com>
2026-01-26 15:22:49 +0100

    cleanup

--- CameraWebServer.ino
+++ CameraWebServer.ino
@@ -18,23 +18,8 @@
 // ===================
 // Select camera model
 // ===================
-//#define CAMERA_MODEL_WROVER_KIT // Has PSRAM
-//#define CAMERA_MODEL_ESP_EYE // Has PSRAM
-//#define CAMERA_MODEL_ESP32S3_EYE // Has PSRAM
-//#define CAMERA_MODEL_M5STACK_PSRAM // Has PSRAM
-//#define CAMERA_MODEL_M5STACK_V2_PSRAM // M5Camera version B Has PSRAM
-//#define CAMERA_MODEL_M5STACK_WIDE // Has PSRAM
-//#define CAMERA_MODEL_M5STACK_ESP32CAM // No PSRAM
-//#define CAMERA_MODEL_M5STACK_UNITCAM // No PSRAM
 #define CAMERA_MODEL_AI_THINKER // Has PSRAM
-//#define CAMERA_MODEL_TTGO_T_JOURNAL // No PSRAM
-//#define CAMERA_MODEL_XIAO_ESP32S3 // Has PSRAM
-// ** Espressif Internal Boards **
-//#define CAMERA_MODEL_ESP32_CAM_BOARD
-//#define CAMERA_MODEL_ESP32S2_CAM_BOARD
-//#define CAMERA_MODEL_ESP32S3_CAM_LCD
-//#define CAMERA_MODEL_DFRobot_FireBeetle2_ESP32S3 // Has PSRAM
-//#define CAMERA_MODEL_DFRobot_Romeo_ESP32S3 // Has PSRAM
+
 #include "camera_pins.h"
 
 // ===========================
@@ -164,6 +149,8 @@ void setup() {
   // ustawienia przekaźnika grzałki
   pinMode(przekaznik_grzalka, OUTPUT);
   digitalWrite(przekaznik_grzalka, HIGH); //wylacz grzałkę na start
+
+  // proby czujnika odleglosci
   // SharpIR splawik = SharpIR(13, 1080);
   // pinMode(czujnik_IR, INPUT);
 
@@ -302,9 +289,5 @@ void loop() {
 
   // zarządzanie grzałką
   check_temps(temperatureC);
-  // digitalWrite(przekaznik_grzalka, HIGH); //włącz grzałkę
-  // delay(200);
-  // digitalWrite(przekaznik_grzalka, LOW); //wyłącz grzałkę
-  // delay(200);
   delay(2000);
 }
